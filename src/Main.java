6c1515617b60c23ee4380e333577a6404dbe12c8
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-21 10:17:31 +0300

    add first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2 ();
+
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 14;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " +age+ " , то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " +age+ " , то он не достиг совершеннолетия, нужно немного подождать");
+
+        }
+
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
     }
 }
