f20e641923312285289f45a0c474618cd9c979aa
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-22 13:51:35 +0300

    add  fourth task

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1 ();
         task2 ();
         task3 ();
+        task4 ();
 
     }
     public static void task1 () {
@@ -33,5 +34,20 @@ public class Main {
         } else {
             System.out.println("Если скорость" + carSpeed + " , то  можно ездить спокойно");
         }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        byte age = 17;
+        if (age >=2 && age <= 6){
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        } else if (age >= 7 && age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        } else if (age >=18 && age <24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в унивеситет");
+        } else if (age >= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
+        }
+
+
     }
 }
