0f2fb835e93a3cc5671ef6aa6815f9ce395a6627
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-23 03:04:31 +0300

    add  sixth task

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3 ();
         task4 ();
         task5 ();
+        task6 ();
 
     }
     public static void task1 () {
@@ -54,11 +55,27 @@ public class Main {
         System.out.println("Задача 5");
         byte age=4;
         if (age < 5){
-            System.out.println("«Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
         }else if (age >= 5 && age < 14){
             System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении");
         }else if (age >14){
             System.out.println("Если возраст ребенка равен " + age + ", то ему без сопровождения взрослого");
+        }
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+        int passangersUplift = 10;
+        byte totalRoom = 102;
+        byte seetingRoom = 60;
+        int standingRoom = (totalRoom - seetingRoom);
+        int placeSeetingPlaces = (seetingRoom - passangersUplift);
+        int placeStandindPlaces = (totalRoom - passangersUplift);
+        if (passangersUplift >= totalRoom){
+            System.out.println("Вагон заполнен полностью, мест нет");
+        }else if (passangersUplift < seetingRoom) {
+            System.out.println("В вагоне осталось " + placeSeetingPlaces + " сидячих и " + standingRoom + " стоячих мест"  );
+        }else if (passangersUplift > seetingRoom && passangersUplift <=totalRoom) {
+            System.out.println("В вагоне осталось " + placeStandindPlaces + " стоячих мест");
         }
     }
 
